48066e3553ba6509bb504d35225e6124472da81d
Kevin Wang <kevinw@cse.ust.hk>
2017-08-08 14:44:16 +0800

    starting code

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -7,35 +7,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HelloController {
     
-	@Autowired
-	private SomeSpringBean myBean;
-	
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!" + myBean.getCounter();
-//    	return "hi";
+    	return "hi";
     }
     
-    @RequestMapping(value="/hi", method=RequestMethod.POST)
-    public Student sayHiToStudent(@RequestParam(value="parameter1", defaultValue="Kevin") String name,
-    		@RequestParam(value="parameter2", defaultValue="") String who 
-    ) {
-    	return new Student(name + " " + who, 3);
-    }
 }
 
-class Student {
-	private String name;
-	private int year;
-	public Student(String name, int year) {
-		this.name = name;
-		this.year = year;
-	}
-	public String getName() {
-		return name;
-	}
-	public int getYear() {
-		return year;
-	}
-	
-}
+// Add the Student class here
